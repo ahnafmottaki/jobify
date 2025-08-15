adfe23ed5993459ef204f6d1e337e6d0c6441f26
ahnafmottaki <ahnafmottaki2022@gmail.com>
2025-08-15 11:45:14 +0600

    add mongoose and connect to database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,15 @@
+import mongoose from "mongoose";
 import app from "./app";
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("server started listening at " + port);
-});
+(async function () {
+  try {
+    await mongoose.connect(process.env.MONGO_URL!);
+    app.listen(port, () => {
+      console.log("server started listening at " + port);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
