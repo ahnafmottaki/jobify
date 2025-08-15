5dd57abd4ee47c14a133b0d3bf8262d782ad5261
ahnafmottaki <ahnafmottaki2022@gmail.com>
2025-08-15 11:52:41 +0600

    add job.model.ts

---
+++ server/src/models/job.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    jobStatus: {
+      type: String,
+      required: true,
+      enum: ["interview", "declined", "pending"],
+    },
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "internship"],
+      default: "full-time",
+    },
+    jobLocation: {
+      type: String,
+      default: "my city",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Job", JobSchema);
