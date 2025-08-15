fac58c1485a6e099d26c5822e880bd8b5bbc92ac
ahnafmottaki <ahnafmottaki2022@gmail.com>
2025-08-15 19:54:53 +0600

    create constants for job model

--- server/src/models/job.model.ts
+++ server/src/models/job.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { type InferSchemaType } from "mongoose";
+import { JOB_STATUS, JOB_TYPE } from "../utils/constants";
 
 const JobSchema = new mongoose.Schema(
   {
@@ -10,13 +11,13 @@ const JobSchema = new mongoose.Schema(
     },
     jobStatus: {
       type: String,
-      enum: ["interview", "declined", "pending"],
-      default: "interview",
+      enum: Object.values(JOB_STATUS),
+      default: JOB_STATUS.PENDING,
     },
     jobType: {
       type: String,
-      enum: ["full-time", "part-time", "internship"],
-      default: "full-time",
+      enum: Object.values(JOB_TYPE),
+      default: JOB_TYPE.FULL_TIME,
     },
     jobLocation: {
       type: String,
