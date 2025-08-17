91e102fbe4b70864737330efee7764f0b0a00423
ahnafmottaki <ahnafmottaki2022@gmail.com>
2025-08-17 23:25:53 +0600

    finish pagiantion in all jobs page

--- client/src/components/JobsContainer.tsx
+++ client/src/components/JobsContainer.tsx
@@ -1,8 +1,12 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useAllJobsContext } from "../pages/AllJobs";
+import PageBtnContainer from "./PageBtnContainer";
 const JobsContainer = () => {
-  const { jobs } = useAllJobsContext();
+  const {
+    jobs,
+    pagination: { totalJobs, numOfPages },
+  } = useAllJobsContext();
   if (jobs.length === 0) {
     return (
       <Wrapper>
@@ -12,11 +16,15 @@ const JobsContainer = () => {
   }
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{totalJobs.length > 0 && "s"}
+      </h5>
       <div className="jobs">
         {jobs.map((job) => (
           <Job key={job._id} {...job} />
         ))}
       </div>
+      {Number(numOfPages) > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
---
+++ client/src/components/PageBtnContainer.tsx
@@ -0,0 +1,124 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { useAllJobsContext } from "../pages/AllJobs";
+import { useLocation, useNavigate } from "react-router";
+
+const PageBtnContainer = () => {
+  const navigate = useNavigate();
+  const {
+    pagination: { numOfPages, currentPage },
+  } = useAllJobsContext();
+  const pages = Array.from(
+    { length: Number(numOfPages) },
+    (_, index) => index + 1
+  );
+  const { search, pathname } = useLocation();
+  console.log(search, pathname);
+  const handlePageChange = (pageNumber: number) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber.toString());
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+  const addPageButton = ({
+    pageNumber,
+    activeClass,
+  }: {
+    pageNumber: number;
+    activeClass: boolean;
+  }) => {
+    return (
+      <button
+        className={`btn page-btn ${activeClass && "active"}`}
+        key={pageNumber}
+        onClick={() => handlePageChange(pageNumber)}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+
+  const renderPageButtons = () => {
+    const pageButtons = [];
+    //first page
+    pageButtons.push(
+      addPageButton({ pageNumber: 1, activeClass: +currentPage === 1 })
+    );
+    // before dots
+    if (+currentPage > 3) {
+      pageButtons.push(
+        <span className="page-btn dots" key={"dots-1"}>
+          ...
+        </span>
+      );
+    }
+    //one before current page
+    if (+currentPage !== 1 && +currentPage !== 2) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: +currentPage - 1,
+          activeClass: false,
+        })
+      );
+    }
+    //current page
+    if (+currentPage !== 1 && +currentPage !== +numOfPages) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: +currentPage,
+          activeClass: true,
+        })
+      );
+    }
+    //one after current page
+    if (+currentPage !== +numOfPages && +currentPage !== +numOfPages - 1) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: +currentPage + 1,
+          activeClass: false,
+        })
+      );
+    }
+    //after dots
+    if (+currentPage < +numOfPages - 2) {
+      pageButtons.push(
+        <span className="page-btn dots" key={"dots-1"}>
+          ...
+        </span>
+      );
+    }
+    //last page
+    pageButtons.push(
+      addPageButton({
+        pageNumber: +numOfPages,
+        activeClass: +currentPage === +numOfPages,
+      })
+    );
+    return pageButtons;
+  };
+
+  return (
+    <Wrapper>
+      <button
+        className="btn prev-btn"
+        onClick={() =>
+          handlePageChange(+currentPage < 2 ? +numOfPages : +currentPage - 1)
+        }
+      >
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">{renderPageButtons()}</div>
+      <button
+        className="btn next-btn"
+        onClick={() =>
+          handlePageChange(+currentPage >= +numOfPages ? 1 : +currentPage + 1)
+        }
+      >
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+
+export default PageBtnContainer;
